fdc477f2f7cebe310af9a45bb845ea82f168ccd8
gabybaty <batistuta9.gb@gmail.com>
2021-11-10 12:43:21 -0300

    vistas news fix

--- controllers/detailControllers.js
+++ controllers/detailControllers.js
@@ -1,22 +1,17 @@
 const fs = require('fs');
 const path = require('path');
-let noticias = require('../data/news_db');
+let noticias = require('../data/detail_db');
 
 module.exports ={
     detail : (req,res) => {   
-        let detailNoticia;
-         for (const individual of noticias) {
-            if (individual.id===req.params.id) {
-                detailNoticia = individual 
-                break;
-            } 
-         }
-         console.log(noticias);
-        console.log(detailNoticia)
+        
+        let noticia = noticias.find(noticia=> noticia.id===+req.params.id);
+         console.log(noticia);
+        
         return res.render('detailnews',{    
             title: 'CIRCULO-DETALLE-NOTICIAS',
-            detailNoticia,
-           
+            noticia,
+            noticias
         })     
         
     },
