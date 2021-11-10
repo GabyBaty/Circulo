5e1ce359572497e068c567f8ff3e4a614bf1f0ac
gabybaty <batistuta9.gb@gmail.com>
2021-11-10 13:58:36 -0300

    ya

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,6 @@ var{index,about,contacto,filiales,bienestar,reserva,tour,farmacia,delegado,evacu
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-router.get('/', index);
 router.get('/about', about);
 router.get('/contactos', contacto);
 router.get('/filial',filiales);
@@ -14,6 +13,10 @@ router.get('/wellness',bienestar);
 router.get('/reservas',reserva);
 router.get('/tours',tour);
 router.get('./farmacias',farmacia);
+router.get('./delegados',delegado);
+router.get('./evacuacion',evacuation);
+router.get('/sorteos',sorteos);
 router.get('/formulario',formulario)
 router.get('/formularios',formularios)
+
 
