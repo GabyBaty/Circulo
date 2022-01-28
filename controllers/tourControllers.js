ed59d5e4f79d7707e2e1e4424a03b64934d331a7
kucki90 <c.kuckiewicz@hotmail.com>
2022-01-28 19:24:19 -0300

    formulario tour

--- controllers/tourControllers.js
+++ controllers/tourControllers.js
@@ -5,6 +5,14 @@ const {form, guardar} = require('../data/turismo_db');
 
 const {validationResult} = require('express-validator');
 
+/* configuracion emailjs */
+const {SMTPClient, Message} = require('emailjs');
+const client = new SMTPClient({
+	user: 'urbaneventsportaleric@gmail.com',
+	password: 'gedeon300',
+	host: 'smtp.gmail.com',
+	ssl: true,
+});
 
 module.exports ={
     tour : (req,res) => {    //tour viene de router//
@@ -48,4 +56,38 @@ module.exports ={
         }
         
     },
+    sendForm : (req,res) => {
+        const {codigo,situacion,documento,cuil,socio,apellido,nombre,cargo,unidad,nacimiento,provincia,localidad,domicilio,cp,telefono,celular} = req.body
+       
+        const message = new Message( {
+            text: 'i hope this works',
+            from: 'formulario.actualizacion@gmail.com',
+            to: 'gabriel.carrizo@cirsubgn.org',
+            subject: 'testing emailjs',
+             attachment: [
+                { data: `
+                <html>
+                    <b>Nombre</b><span>${q3_nombreCompleto3[first]}<span/>
+                    <b>Apellido</b><span>${q3_nombreCompleto3[last]}<span/>
+                    <b>Documento de identidad</b><span>${documento}<span/>
+                    <b>Código estadístico</b><span>${codigo}<span/>
+                    <b>Email</b><span>${q4_correoElectronico4}<span/>
+                    <b>Teléfono</b><span>${q5_numeroDe5[telefono]}<span/>
+                    <b>Fecha de llegada</b><span>${fecha_llegada}<span/>
+                    <b>Fecha de salida</b><span>${fecha_salida}<span/>
+                    <b>Destino turístico</b><span>${q5_numeroDe5[area]}<span/>
+                    <b>Adultos</b><span>${q24_adultos}<span/>
+                    <b>Menores de 12 años</b><span>${q25_12Anos}<span/>
+                    <b>Menores de 3 años</b><span>${q26_3Anos}<span/>
+                    <b>Notas y peticiones especiales</b><span>${q13_notasY}<span/>
+                    
+                </html>`, 
+                alternative: true },
+            ], 
+        });
+        client.send(message, (err, message) => {
+            console.log(err || message);
+        });
+        return res.redirect('/')
+    }
 }
