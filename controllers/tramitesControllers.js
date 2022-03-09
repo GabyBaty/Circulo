b7204f2aacfdadfef094ade31339bbdb7a9a94af
gabybaty <batistuta9.gb@gmail.com>
2022-03-08 23:46:48 -0300

    forms

--- controllers/tramitesControllers.js
+++ controllers/tramitesControllers.js
@@ -58,4 +58,65 @@ module.exports ={
         return res.redirect('/')
     },
     
+/* 
+    sendForm2 : (req,res) => {
+        const {codigo,situacion,alta,apellido,nombre,socio,parentesco,apellido2,nombres2,dni2,nacimiento} = req.body
+       
+        const message = new Message( {
+            text: 'i hope this works',
+            from: 'formulario.actualizacion@gmail.com',
+            to: 'gabriel.carrizo@cirsubgn.org',
+            cc: 'paginacirsub@cirsubgn.org',
+            subject: 'Familiares a cargo',
+             attachment: [
+                { data: `
+                <html>
+                    <b>Código estadístico</b><span>${codigo}<span/><br> 
+                    <b>Situación</b><span>${situacion}<span/><br> 
+                    <b>Fecha de Alta</b><span>${alta}<span/><br> 
+                    <b>Apellido</b><span>${apellido}<span/><br> 
+                    <b>Nombre</b><span>${nombre}<span/><br> 
+                    <b>Número de socio</b><span>${socio}<span/><br> 
+                    <b>Parentesco</b><span>${parentesco}<span/><br> 
+                    <b>Apellido</b><span>${apellido2}<span/><br> 
+                    <b>Nombres</b><span>${nombres2}<span/><br> 
+                    <b>DNI</b><span>${dni2}<span/><br> 
+                    <b>Fecha de nacimiento</b><span>${nacimiento}<span/><br>
+                    <b>Número de socio</b><span>${socio}<span/><br> 
+                    <b>Parentesco</b><span>${parentesco}<span/><br> 
+                    <b>Apellido</b><span>${apellido2}<span/><br> 
+                    <b>Nombres</b><span>${nombres2}<span/><br> 
+                    <b>DNI</b><span>${dni2}<span/><br> 
+                    <b>Fecha de nacimiento</b><span>${nacimiento}<span/><br>
+                    <b>Número de socio</b><span>${socio}<span/><br> 
+                    <b>Parentesco</b><span>${parentesco}<span/><br> 
+                    <b>Apellido</b><span>${apellido2}<span/><br> 
+                    <b>Nombres</b><span>${nombres2}<span/><br> 
+                    <b>DNI</b><span>${dni2}<span/><br> 
+                    <b>Fecha de nacimiento</b><span>${nacimiento}<span/><br>
+                    <b>Número de socio</b><span>${socio}<span/><br> 
+                    <b>Parentesco</b><span>${parentesco}<span/><br> 
+                    <b>Apellido</b><span>${apellido2}<span/><br> 
+                    <b>Nombres</b><span>${nombres2}<span/><br> 
+                    <b>DNI</b><span>${dni2}<span/><br> 
+                    <b>Fecha de nacimiento</b><span>${nacimiento}<span/><br>
+                    <b>Número de socio</b><span>${socio}<span/><br> 
+                    <b>Parentesco</b><span>${parentesco}<span/><br> 
+                    <b>Apellido</b><span>${apellido2}<span/><br> 
+                    <b>Nombres</b><span>${nombres2}<span/><br> 
+                    <b>DNI</b><span>${dni2}<span/><br> 
+                    <b>Fecha de nacimiento</b><span>${nacimiento}<span/><br> 
+                  
+
+                </html>`, 
+                alternative: true },
+            ], 
+        });
+        client.send(message, (err, message) => {
+            console.log(err || message);
+        });
+        return res.redirect('/')
+    }, */
+    
+
 }
